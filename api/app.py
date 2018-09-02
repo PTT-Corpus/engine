c86c1c4cd5eda08c5fede36417b451577012773e
amigcamel <amigcamel@gmail.com>
2018-09-02 23:42:11 +0800

    add Query (/query)

--- api/app.py
+++ api/app.py
@@ -1,18 +1,26 @@
 """PTT Engine API."""
 import falcon
 
+from funcs import query
 
-class Test:
+
+class Query:
     """Test."""
 
     def on_get(self, req, resp):
         """GET method."""
-        output = {
-            'name': 'Aji',
-            'message': 'this is a test'
-        }
+        word = req.get_param('word', required=True)
+        page = req.get_param_as_int('page') or 0
+        size = req.get_param_as_int('size') or 10
+        post_type = req.get_param_as_int('post_type')
+        board = req.get_param('board', default='movie')
+        output = query(word=word,
+                       page=page,
+                       size=size,
+                       post_type=post_type,
+                       board=board)
         resp.media = output
 
 
 api = falcon.API()
-api.add_route('/', Test())
+api.add_route('/query', Query())
--- api/funcs.py
+++ api/funcs.py
@@ -1,11 +1,40 @@
 """Core functions."""
-import os
+from elasticsearch_dsl import (
+    Search,
+    Q,
+)
 
-from elasticsearch import Elasticsearch
+from settings import get_client
 
 
-def get_client():
-    """Get Elasticsearch client."""
-    return Elasticsearch(hosts=[
-        '{ELASTICSEARCH_HOST}:{ELASTICSEARCH_PORT}'.format(**os.environ)]
+client = get_client()
+
+
+def query(word: str,
+          page: int,
+          size: int,
+          post_type: int,
+          board: str) -> dict:
+    """Query word."""
+    s = Search(using=client, index='ptt')
+    s.query = Q(
+        'bool',
+        must=[
+            Q('match', content=word),
+            Q('match', post_type=post_type),
+            Q('match', board=board),
+        ]
     )
+    total = s.count()
+    start = page * size
+    end = start + size
+    output = {
+        'total': total,
+        'data': [
+            i.to_dict()
+            for i
+            in s[start:end]
+            if total
+        ]
+    }
+    return output
